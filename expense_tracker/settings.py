4d560322a767671bae07ca58dfd5d43b3b5470a0
MohitS3thi <164456349+MohitS3thi@users.noreply.github.com>
2026-04-01 18:14:14 +0530

    render

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -104,7 +104,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+# Use an absolute static URL so asset links work on nested routes (e.g. /login/).
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
